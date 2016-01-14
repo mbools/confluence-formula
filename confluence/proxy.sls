simonwgill/bitbucket-formula

Changed proxy settings to use CentOS package names and apache config layout.
Added SELinux configuration for use on default CentOS 7 Minimal Install.

jira/proxy.sls
@@ -4,31 +4,59 @@
 apache:
   pkg.installed:
     - pkgs:
-       - apache2
+       - httpd
   service.running:
-    - name: apache2
+    - name: httpd
     - enable: True
     - require:
-      - pkg: apache
+      - pkg: apache 
 
 apache-restart:
   module.wait:
     - name: service.restart
-    - m_name: apache2
+    - m_name: httpd
 
 apache-reload:
   module.wait:
     - name: service.reload
-    - m_name: apache2
+    - m_name: httpd
+
+jira-sites-enabled:
+  file.directory:
+    - name: /etc/httpd/sites-enabled
+    - user: apache
+    - group: apache
+    - mode: 755 
+    - require_in:
+      - file: enable-jira-site
+
+jira-sites-available:
+  file.directory:
+      - name: /etc/httpd/sites-available
+      - user: apache
+      - group: apache
+      - mode: 755
+
+jira-sites-available-config:
+  file.blockreplace:
+      - name: /etc/httpd/conf/httpd.conf
+      - marker_start: '#BEGIN Managed by salt - do not edit'
+      - marker_end: '#END Managed by salt - do not edit'
+      - content: 'IncludeOptional sites-enabled/*.conf'
+      - append_if_not_found: True
+      - require:
+        - pkg: apache
 
 jira-vhost:
   file.managed:
-    - name: /etc/apache2/sites-available/jira
+    - name: /etc/httpd/sites-available/jira.conf
     - source: salt://jira/templates/jira-vhost.tmpl
+    - require:
+      - file: jira-sites-available
 
 disable-default-site:
   file.absent:
-    - name: /etc/apache2/sites-enabled/000-default
+    - name: /etc/httpd/sites-enabled/000-default
     - listen_in:
       - module: apache-reload
     - require:
@@ -36,12 +64,17 @@ disable-default-site:
 
 enable-jira-site:
   file.symlink:
-    - name: /etc/apache2/sites-enabled/jira
-    - target: /etc/apache2/sites-available/jira
+    - name: /etc/httpd/sites-enabled/jira.conf
+    - target: /etc/httpd/sites-available/jira.conf
     - listen_in:
       - module: apache-reload
     - require:
       - file: jira-vhost
+
+httpd_can_network_connect:
+  selinux.boolean:
+    - value: True
+    - persist: True
  
 a2enmod proxy:
   cmd.wait:

mbools/confluence-formula

confluence/proxy.sls
@@ -4,31 +4,59 @@
 apache:
   pkg.installed:
     - pkgs:
-       - apache2
+       - httpd
   service.running:
-    - name: apache2
+    - name: httpd
     - enable: True
     - require:
-      - pkg: apache
+      - pkg: apache 
 
 apache-restart:
   module.wait:
     - name: service.restart
-    - m_name: apache2
+    - m_name: httpd
 
 apache-reload:
   module.wait:
     - name: service.reload
-    - m_name: apache2
+    - m_name: httpd
+
+confluence-sites-enabled:
+  file.directory:
+    - name: /etc/httpd/sites-enabled
+    - user: apache
+    - group: apache
+    - mode: 755 
+    - require_in:
+      - file: enable-confluence-site
+
+confluence-sites-available:
+  file.directory:
+      - name: /etc/httpd/sites-available
+      - user: apache
+      - group: apache
+      - mode: 755
+
+confluence-sites-available-config:
+  file.blockreplace:
+      - name: /etc/httpd/conf/httpd.conf
+      - marker_start: '#BEGIN Managed by salt - do not edit'
+      - marker_end: '#END Managed by salt - do not edit'
+      - content: 'IncludeOptional sites-enabled/*.conf'
+      - append_if_not_found: True
+      - require:
+        - pkg: apache
 
 confluence-vhost:
   file.managed:
-    - name: /etc/apache2/sites-available/confluence
+    - name: /etc/httpd/sites-available/confluence.conf
     - source: salt://confluence/templates/confluence-vhost.tmpl
+    - require:
+      - file: confluence-sites-available
 
 disable-default-site:
   file.absent:
-    - name: /etc/apache2/sites-enabled/000-default
+    - name: /etc/httpd/sites-enabled/000-default
     - listen_in:
       - module: apache-reload
     - require:
@@ -36,12 +64,17 @@ disable-default-site:
 
 enable-confluence-site:
   file.symlink:
-    - name: /etc/apache2/sites-enabled/confluence
-    - target: /etc/apache2/sites-available/confluence
+    - name: /etc/httpd/sites-enabled/confluence.conf
+    - target: /etc/httpd/sites-available/confluence.conf
     - listen_in:
       - module: apache-reload
     - require:
       - file: confluence-vhost
+
+httpd_can_network_connect:
+  selinux.boolean:
+    - value: True
+    - persist: True
  
 a2enmod proxy:
   cmd.wait: